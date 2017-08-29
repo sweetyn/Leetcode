f72338e7fda45c7c9aa6157e60e18a4fa7333710
sweetyn <sweetyn731@icloud.com>
2017-08-29 16:43:24 -0700

    Ver1

---
+++ validAnagram.py
@@ -0,0 +1,22 @@
+"""Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false."""
+
+def vaildAnagram(s, t):
+
+    for x in s:
+        if x in t:
+            t.replace(x, "")
+        else:
+            return False
+
+    return True
+
+
+s = "rat"
+t = "tar"
+
+print (vaildAnagram(s, t))
+#complted in 7mins
