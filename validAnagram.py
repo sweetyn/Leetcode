08956cafd37262014eeb30fb3179a72450c5f8ea
sweetyn <sweetyn731@icloud.com>
2017-08-29 17:17:12 -0700

    v2

--- validAnagram.py
+++ validAnagram.py
@@ -1,4 +1,5 @@
-"""Given two strings s and t, write a function to determine if t is an anagram of s.
+"""242. Valid Anagram
+Given two strings s and t, write a function to determine if t is an anagram of s.
 
 For example,
 s = "anagram", t = "nagaram", return true.
