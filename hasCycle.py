d79a1b06a9f8866c8341475a6ea4d2c12a383f21
sweetyn <sweetyn731@icloud.com>
2018-01-14 20:31:04 -0800

    linkedList hasCycle

---
+++ hasCycle.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head:
+            return False
+        else:
+            head.next()
+            return True
+
+
+        return False
+
+
+a = []
+a.append
